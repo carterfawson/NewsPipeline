5c02a46fefe80256caf0d654de5d89154cae94bc
Carter Fawson <carteraaronfawson@gmail.com>
2017-01-16 19:43:24 -0600

    Added the gitignore

--- NewsOrchestrator.py
+++ NewsOrchestrator.py
@@ -6,10 +6,7 @@ from multiprocessing import Pool
 sources = NewsAPIConnection.retrieveNewsSources()
 
 pool = Pool()
-#pool.map(NewsStreamer.SendArticleToStream, articles['articles'])
 
 for source in sources:
     articles = NewsAPIConnection.retrieveRecentNews(source['id'])
-    #for article in articles['articles']:
-        #NewsStreamer.SendArticleToStream(article)
     pool.map(NewsStreamer.SendArticleToStream, articles['articles'])
